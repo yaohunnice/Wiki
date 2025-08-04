737e3f5d07ef6f77f48f714f584c7cca61794763
yaohunnice <159331462+yaohunnice@users.noreply.github.com>
2025-08-04 10:32:29 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '鬥魂帝國',
+  tagline: '玩法介紹及攻略',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
